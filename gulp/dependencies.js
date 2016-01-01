8bed3a41c6c6181b6d7299e51e80779a594f203c
webyom <webyom@gmail.com>
2016-01-01 23:17:33 +0800

    update

--- gulp/dependencies.js
+++ gulp/dependencies.js
@@ -3,7 +3,6 @@ var gulp = require('gulp');
 // move dependencies into build dir
 gulp.task('dependencies', function () {
   return gulp.src([
-    'node_modules/traceur/bin/traceur-runtime.js',
     'node_modules/yom-require/src/require.js',
     'node_modules/@reactivex/rxjs/dist/global/Rx.umd.js',
     'node_modules/angular2/bundles/angular2-polyfills.js',
--- src/js/app/boot.js
+++
@@ -1,6 +0,0 @@
-var browser_1 = require('angular2/platform/browser');
-var router_1 = require('angular2/router');
-var app_component_1 = require('./app.component');
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {};
-browser_1.bootstrap(app_component_1.default, [router_1.ROUTER_PROVIDERS]);
